2829efc80917b532dd589577abbd8b106f4f147a
Juan Jara <jaracalle2041@gmail.com>
2023-05-15 15:37:01 -0500

    Retoques finales a la arquitectura del aplicativo

--- interfaz/index.py
+++ interfaz/index.py
@@ -1,6 +1,7 @@
 # Importaciones
 import interfaz.registro as gui
 import customtkinter as ctk
+import interfaz.face as face
 import os
 from PIL import ImageTk, Image
 
@@ -68,6 +69,13 @@ class Index:
 
 
 class Asistencia:
+    # Boton volver atrás
+    def btn_volver_atras(self):
+        # Destuir ventana principal
+        self.root.destroy()
+        # Instanciación de la ventana registro
+        ventana_index = Index()
+
     def __init__(self):
         # Creación de la ventana principal
         self.root = ctk.CTk()
@@ -75,6 +83,39 @@ class Asistencia:
         self.root.iconbitmap(os.path.join(carpeta_imagenes, "logo.ico"))
         self.root.geometry("450x300")
         self.root.resizable(False,False)
+        
+        # Contenido de la ventana
+        # Frame principal
+        self.frame = ctk.CTkFrame(master=self.root, width=400, height=250)
+        self.frame.grid(row=0, column=0, padx= 20, pady=20, sticky="nsew")
+        #Frame del centro
+        self.right_frame = ctk.CTkFrame(master=self.root)
+        self.right_frame.grid(row=1, column=1, sticky="nsew")
+
+        # Botón de volver atrás
+        self.icon = ctk.CTkImage(Image.open(os.path.join(carpeta_imagenes, "volver.png")))
+        ctk.CTkButton(self.frame, image=self.icon, text="",corner_radius=50, width=32, height=32, command=self.btn_volver_atras).grid(row=0, column=0)
+
+        # Contenido del centro
+        # Frame superior del interior
+        self.titulo_frame = ctk.CTkFrame(master=self.right_frame)
+        self.titulo_frame.grid(row=0, column=0, padx=40, pady=10)
+        # -> Titulos
+        self.titulo = ctk.CTkLabel(self.titulo_frame, text="Hermes",font=("CTkFont",  20))
+        self.titulo.grid(row=0, column=0, pady=0)
+        self.titulo = ctk.CTkLabel(self.titulo_frame, text="Sistema de reconocimiento facial")
+        self.titulo.grid(row=1, column=0)
+        # Frame centro del interior
+        self.form_frame = ctk.CTkFrame(master=self.right_frame)
+        self.form_frame.grid(row=1, column=0, pady=30)
+        # -> Formulario
+        # --> Nombre
+        self.nombre_entry = ctk.CTkEntry(master=self.form_frame, placeholder_text="Nombre de la clase")
+        self.nombre_entry.grid(row=0, column=0)
+        # Botón de enviar formulario
+        ctk.CTkButton(self.right_frame, text="Comenzar toma de asistencia", command=face.main).grid(row=2, column=0, pady=10)
+
+        
 
         # Contenido de la ventana
         self.root.mainloop()
